df307c1cc118e2526ced09a5211aec82aef5ec52
lijinpu <lijinpu19990306@gmail.com>
2023-04-07 14:58:27 +0800

    auto update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-row relative justify-between w-full">
       <div className="flex flex-col relative w-full">
-        <h1 className="font-extrabold text-2xl tracking-tight mb-4">Jinpu Li</h1>
+        <h1 className="font-extrabold text-2xl tracking-tight mb-4">Jinpu Li (李金璞)</h1>
         <p className="text-gray-600 dark:text-gray-400 mb-4">
           PhD student in Economics
         </p>
