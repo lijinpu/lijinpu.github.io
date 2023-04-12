23215c3f59f7ae68690345d9701af155a6a25e8d
lijinpu <lijinpu19990306@gmail.com>
2023-04-12 14:45:18 +0800

    auto update

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,10 +11,10 @@ export default function About() {
 
         <h2>Education</h2>
         <p>
-        2021/09 - 2026 (Exp.), 清华大学, 理论经济学, 博士
+        2022&nbsp;&nbsp;&nbsp;<b>Ph.D. candidate in Economics</b>, Tsinghua University
         </p>
         <p>
-        2017/09 - 2021/06,     清华大学, 经济学, 学士
+        2021&nbsp;&nbsp;&nbsp;<b>B.A. in Economics</b>, Tsinghua University
         </p>
 
         <h2>Publications</h2>
@@ -29,9 +29,19 @@ export default function About() {
         <p>
         “企业数据资产化：会计确认与价值评估”, 罗玫, 李金璞, 汤珂, 2022
         </p>
-        <h2>Books</h2>
+        <h2>Books & Book Chapters</h2>
         <p>
           “数据经济学”, 汤珂, 熊巧琴, 李金璞, 屈阳, <b>清华大学出版社</b>, 2023
+        </p>
+        <h2>Teaching Assistant</h2>
+        <p>
+        Principles of Economics (00510454) by Prof. Feng Dong. 2023 Spring.
+        </p>
+        <p>
+        Econometrics II (30700932) by Prof. Sainan Jin. 2023 Spring, and 2022 Spring.
+        </p>
+        <p>
+        Introduction to Financial Economics (40701113) by Prof. Ke Tang. 2022 Fall, 2021 Fall , and 2020 Fall.
         </p>
         <h2>Rewards</h2>
         <p>
@@ -39,6 +49,21 @@ export default function About() {
         </p>  
         <p>
         清华大学优秀本科毕业论文 (Outstanding Undergraduate Thesis, Tsinghua University), 2021
+        </p>
+        <p>
+        清华大学优秀共青团员 (Excellent League Member, Tsinghua University), 2021
+        </p>
+        <p>
+        唐立新奖学金综合优秀奖 (Tang Lixin Scholarship), 2020
+        </p>
+        <p>
+        清华大学学业优秀奖 (Scholarship of Academic Excellence, Tsinghua University), 2019
+        </p>
+        <p>
+        清华大学文艺优秀奖 (Scholarship of Outstanding Art Performance, Tsinghua University), 2018-2021
+        </p>
+        <p>
+        清华大学社会工作优秀奖 (Scholarship of Social Work, Tsinghua University), 2018
         </p>
       </div>
     </div>
