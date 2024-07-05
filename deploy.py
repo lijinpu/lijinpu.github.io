877a24748e6eabc1e0b28592714bbb35a77af3ab
lijinpu <lijinpu19990306@gmail.com>
2024-07-05 16:34:04 +0800

    auto update

--- deploy.py
+++ deploy.py
@@ -1,12 +1,28 @@
 from git import Repo
 import os
 
-dirfile = os.path.abspath('') # code的文件位置，我默认将其存放在根目录下
+# 获取代码文件的绝对路径
+dirfile = os.path.abspath('')
+
+# 初始化Repo对象
 repo = Repo(dirfile)
 
+# 获取git命令对象
 g = repo.git
+
+# 添加所有更改到暂存区
 g.add("--all")
-g.commit("-m auto update")
+
+# 检查工作区是否有更改
+if repo.is_dirty(untracked_files=True):
+    g.commit("-m", "auto update")
+else:
+    print("No changes to commit.")
+
+# 拉取最新代码
 g.pull()
+
+# 推送本地提交到远程仓库
 g.push()
+
 print("Successful push!")
