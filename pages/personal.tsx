d7e944c3ed1c4885d281d0147f0be695869faef6
lijinpu <lijinpu19990306@gmail.com>
2025-06-11 16:00:17 -0700

    auto update

--- pages/personal.tsx
+++ pages/personal.tsx
@@ -9,7 +9,7 @@ export default function PersonalPage() {
         title="Saxophone"
         description={
           <>
-            I'm an amateur saxophonist. I joined the Tsinghua University Symphonic Band (a.k.a. THUMB) in the fall of 2017, where I served as principal saxophonist and band captain. I've played soprano, alto, and tenor saxophones. Here are selected videos: 
+            I&apos;m an amateur saxophonist. I joined the Tsinghua University Symphonic Band (a.k.a. THUMB) in the fall of 2017, where I served as principal saxophonist and band captain. I&apos;ve played soprano, alto, and tenor saxophones. Here are selected videos:
           </>
         }
       />
@@ -19,7 +19,7 @@ export default function PersonalPage() {
         title="Go (Weiqi)"
         description={
           <>
-            I'm an amateur Go player. I started learning Go at age 7 and reached an amateur 5-dan level. I also participated in the National Professional Dan Qualification Tournament. In 2017, I was lucky enough to place 5th in Tsinghua University's Go competition.
+            I&apos;m an amateur Go player. I started learning Go at age 7 and reached an amateur 5-dan level. I also participated in the National Professional Dan Qualification Tournament. In 2017, I was lucky enough to place 5th in Tsinghua University&apos;s Go competition.
           </>
         }
       />
