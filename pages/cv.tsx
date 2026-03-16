dfd9de0b620ad96160f1454a6dc443f0452c5031
lijinpu <lijinpu19990306@gmail.com>
2026-03-16 15:06:32 +0800

    auto update

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -45,8 +45,10 @@ export default function CV({ lastUpdateISO }: InferGetStaticPropsType<typeof get
 
           <h2>Publications &nbsp;&nbsp; 学术发表</h2>
           <ul>
-            <li>李金璞, 孙浩宁*, 汤珂, 董丰. 数据资产泡沫：理论框架与政策权衡[J]. 管理世界, 2025, 41(09): 1-24.</li>
-            <li>罗玫, 李金璞, 汤珂*. 企业数据资产化：会计确认与价值评估[J]. 清华大学学报(哲学社会科学版), 2023, 38(05): 195-209+226.</li>
+            <li>李金璞, 孙浩宁*, 汤珂, 董丰. 数据资产泡沫：理论框架与政策权衡[J]. 管理世界, 2025, 41(09): 1-24. [注：《社会科学文摘》2025年11期转载]</li>
+            <li>孙浩宁, 李金璞*, 董丰. 人工智能时代的收入分配与宏观治理——基于任务替代与代际交叠的一般均衡分析[J]. 经济学(季刊), 2026, 待刊.</li>
+            <li>李金璞, 孙浩宁*, 董丰, 汤珂. 数字时代宏观经济学的研究新进展[J]. 经济学动态, 2026, 待刊.</li>
+            <li>罗玫, 李金璞, 汤珂*. 企业数据资产化：会计确认与价值评估[J]. 清华大学学报(哲学社会科学版), 2023, 38(05): 195-209+226. [注：人大复印报刊资料《财务与会计导刊（理论版）》2023年12期全文转载]</li>
             <li>李金璞, 汤珂*. 论数据要素市场参与者的培育[J]. 西安交通大学学报(社会科学版), 2023, 43(04): 78-89.</li>
             <li>Dong, F., Li, J.*, Sun, H., & Wang, S. (2025). Dynamic Efficiency Redux: Evidence from China. <i>China & World Economy</i>, 33(05), 196-222.</li>
             <li>
@@ -60,9 +62,6 @@ export default function CV({ lastUpdateISO }: InferGetStaticPropsType<typeof get
 
           <h2>Working Papers &nbsp;&nbsp; 工作论文</h2>
           <ul>
-            <li>人工智能时代的收入分配与政策权衡：基于任务与代际动态的理论分析, 2025. 合作者：孙浩宁, 董丰.</li>
-            <li>数据能力与企业创新：来自千万条招聘信息的证据, 2025. 合作者：赵慧, 韦楚涵, 汤珂.</li>
-            <li>数字时代的宏观经济学的研究新进展, 2024. 合作者：孙浩宁, 汤珂, 董丰.</li>
             <li>
               Macroeconomics of Artificial Intelligence: A Review and Agenda. Feng Dong, Jinpu Li, and Haoning Sun, 2026.
             </li>
@@ -75,9 +74,10 @@ export default function CV({ lastUpdateISO }: InferGetStaticPropsType<typeof get
 
           <h2>Books & Book Chapters &nbsp;&nbsp; 专著及书目章节</h2>
           <ul>
-            <li>数据要素的可信流通. 汤珂和吴志雄主编, 人民出版社, 2024年6月. 撰写第三章《数据可信流通框架》.</li>
-            <li>数据资产化. 汤珂主编, 人民出版社, 2023年8月. 撰写第二章《数据资产的确认》, 第五章《数据资产价值评估》.</li>
+            <li>李金璞, 汤珂. 数据的可信流通：一个理论框架[A]//江小涓主编 (《比较》编辑部主办). 数字经济发展与治理. 北京: 中信出版集团, 2025: 107–120.</li>
             <li>数据经济学. 汤珂, 熊巧琴, 李金璞, 屈阳, 清华大学出版社, 2023年1月第1版, 2024年1月第2版.</li>
+            <li>数据要素的可信流通. 汤珂和吴志雄主编, 人民出版社, 2024年6月. 撰写第三章《数据可信流通框架》.</li>
+            <li>数据资产化. 汤珂主编, 人民出版社, 2023年8月. 撰写第二章《数据资产的确认》, 第五章《数据资产价值评估》.</li> 
           </ul>
 
           <h2>Grants & Rewards &nbsp;&nbsp; 资助与获奖</h2>
@@ -128,7 +128,6 @@ export default function CV({ lastUpdateISO }: InferGetStaticPropsType<typeof get
           <h2>Other Publications &nbsp;&nbsp; 其他发表</h2>
           <ul>
            <li>Gao, R., Xiao, F., Li, J., & Cui, S.* (2026). Textual Semantics and Machine Learning Methods for Data Product Pricing. <i>The Review of Mathematical Economics</i>, 1(01), 129–167. DOI: 10.4310/RME.260221154937</li>
-           <li>李金璞, 汤珂. 数据的可信流通：一个理论框架[A]//江小涓主编 (《比较》编辑部主办). 数字经济发展与治理. 北京: 中信出版集团, 2025: 107–120.</li>
            <li>李金璞, 汤珂. 加快培育统一的数据市场[J]. 清华金融评论, 2025, (06):39-41. </li>
            <li>黄京磊, 李金璞, 汤珂*. 数据信托：可信的数据流通模式[J]. 大数据, 2023, 9(02): 67-78. [注：期刊2023年高被引论文]</li>
            <li>赵丽芳, 林立, 李金璞. 基于数据要素价值链评估企业数据资产[J]. 企业管理, 2023, (12): 88-91.</li> 
