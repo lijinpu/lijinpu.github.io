f3a1dd2bcc54e00f0a94484a1b2b2394ef15c723
lijinpu <lijinpu19990306@gmail.com>
2026-08-04 18:57:08 +0800

    auto update

--- pages/teaching.tsx
+++ pages/teaching.tsx
@@ -5,24 +5,26 @@ export default function Teaching() {
         <h1>Teaching &nbsp;&nbsp; 教学</h1>
         <div>
         <h2>Teaching Assistant at Tsinghua &nbsp;&nbsp; 助教经历@清华大学</h2>  
-        <p>
-        Academic Research Training for Xuetang Class (40512742) by Prof. Feng Dong & Binzhen Wu. 2026 Summer.
-        </p>
-        <p>
-        Econometrics I (30700922) by Prof. Ke Tang. 2025 Fall.
-        </p>
-        <p>
-        Intermediate Macroeconomics (30510763) by Prof. Feng Dong. 2025 Fall. 
-        </p>
-        <p>
-        Principles of Economics (00510454) by Prof. Feng Dong. 2026 Spring, 2025 Spring, 2024 Spring, and 2023 Spring.
-        </p>
-        <p>
-        Econometrics II (30700932) by Prof. Sainan Jin. 2024 Spring, 2023 Spring, and 2022 Spring.
-        </p>
-        <p>
-        Introduction to Financial Economics (40701113) by Prof. Ke Tang. 2022 Fall, 2021 Fall , and 2020 Fall.
-        </p>
+        <ul>
+          <li>
+            Academic Research Training for Xuetang Class (40512742) by Prof. Feng Dong & Binzhen Wu. 2026 Summer.
+          </li>
+          <li>
+            Econometrics I (30700922) by Prof. Ke Tang. 2025 Fall.
+          </li>
+          <li>
+            Intermediate Macroeconomics (30510763) by Prof. Feng Dong. 2025 Fall. 
+          </li>
+          <li>
+            Principles of Economics (00510454) by Prof. Feng Dong. 2026 Spring, 2025 Spring, 2024 Spring, and 2023 Spring.
+          </li>
+          <li>
+            Econometrics II (30700932) by Prof. Sainan Jin. 2024 Spring, 2023 Spring, and 2022 Spring.
+          </li>
+          <li>
+            Introduction to Financial Economics (40701113) by Prof. Ke Tang. 2022 Fall, 2021 Fall , and 2020 Fall.
+          </li>
+        </ul>
         </div>
     </div>
           
